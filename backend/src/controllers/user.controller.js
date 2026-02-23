ab573e2cda321f194a547e77d3ac6717ffb8fff0
zeeze <abdulazeezakindolapo@gmail.com>
2026-02-23 12:10:19 +0100

    200 & 500 status

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -65,9 +65,14 @@ const logoutUser = async (req, res) => {
 
         if(!user) return res.status(400).json({message: "User does not exist"});
 
-        res.status
-    } catch (error) {
+        res.status(200).json({
+            message: "Logout successful"
+        });
         
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error", error
+        })
     }
 }
 
