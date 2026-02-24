51eba95cb9b61f6798dbc1c176da5cb04b364aa6
zeeze <abdulazeezakindolapo@gmail.com>
2026-02-24 16:17:41 +0100

    read and update

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -26,4 +26,50 @@ const createPost = async (req, res) => {
   }
 };
 
-export { createPost };
+//read all posts
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
+
+//update a post
+const updatePost = async (req, res) => {
+  try {
+    // basic validation to check if the body is empty
+
+    //{name: x, description: y, age: z} -> [name, description, age]
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "No data provided for update",
+      });
+    }
+
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!post)
+      return res.status(404).json({
+        message: "Post not found",
+      });
+
+    res.status(200).json({
+      message: "Post updated successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
+
+export { createPost, getPosts, updatePost };
