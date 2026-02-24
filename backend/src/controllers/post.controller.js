b10d40562fb4b6561df8f99f6f6988252b63f67b
zeeze <abdulazeezakindolapo@gmail.com>
2026-02-24 15:36:05 +0100

    post router

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -7,21 +7,23 @@ const createPost = async (req, res) => {
 
     if (!name || !description || !age) {
       return res.status(400).json({ message: "All fields are required" });
-
-      const post = await Post.create({
-        name,
-        description,
-        age,
-      });
-
-      res.status(201).json({
-        message: "Post created successfully",
-        post
-      });
     }
+    const post = await Post.create({
+      name,
+      description,
+      age,
+    });
+
+    res.status(201).json({
+      message: "Post created successfully",
+      post,
+    });
   } catch (error) {
     res.status(500).json({
-        message: "Internal Server Error", error
-    })
+      message: "Internal Server Error",
+      error,
+    });
   }
 };
+
+export { createPost };
