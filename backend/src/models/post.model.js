80ab12dcb3eaf9d109490d4c9b1efa6754413311
zeeze <abdulazeezakindolapo@gmail.com>
2026-02-24 15:19:37 +0100

    post

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -1,12 +1,29 @@
 import mongoose, { Schema } from "mongoose";
 
-const postSchema = new Schema (
-    {
-
+const postSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true
-    }
-)
 
-export const Post = mongoose.model("Post", postSchema)
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    age: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 150
+    }
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Post = mongoose.model("Post", postSchema);
